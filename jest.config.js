0f7d3d42a2824018b4358f6bc871d008f8ff73f7
Andrew Vo-Nguyen <andrew.vo89@gmail.com>
2022-07-18 13:02:29 +1000

    build(jest): change jest config file to typescript

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    verbose: true,
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-    },
-};
-exports.default = config;
