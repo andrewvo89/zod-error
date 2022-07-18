913ed0bcf5e0b4c9498422bef7fd8297c067aaf0
Andrew Vo-Nguyen <andrew.vo89@gmail.com>
2022-07-18 12:48:35 +1000

    build(jest): remove jest config file from tsconfig

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+    verbose: true,
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+};
+exports.default = config;
